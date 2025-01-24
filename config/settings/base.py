5a1ec1cafd96993b031728940282044ec168b73d
wkdsh21 <wkdsh21@github.com>
2025-01-24 13:46:21 +0900

    add drf, drf-plugin

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "content.apps.ContentConfig",
     "user.apps.UserConfig",
     "tarot.apps.TarotConfig",
+    # third_apps
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
