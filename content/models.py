b9c3e274fc8f6dde7f53d2c0d2ce6d2a29b9050b
DevMon724 <devmon724@gmail.com>
2025-01-31 13:41:45 +0900

    content-develop merge

--- content/models.py
+++ content/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Review(BaseModel):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    img_url = models.CharField(max_length=255)
+    on_main = models.BooleanField(default=False)
+    view_count = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    # Other fields...
+
+
+class Notice(BaseModel):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    img_url = models.CharField(max_length=255)
+    order = models.IntegerField(default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notices')
+    # Other fields...
+
+
+class Notification(BaseModel):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    img_url = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    # Other fields...
+
+
+class NotiUser(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notiusers')
+    notification = models.ForeignKey(Notification, on_delete=models.CASCADE, related_name='notiusers')
+    is_read = models.BooleanField(default=False)
+
+
+class FAQ(BaseModel):
+    question = models.TextField()
+    answer = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='faqs')
