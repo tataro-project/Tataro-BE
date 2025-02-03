619f9ca6996e534882526a91242874e1b24c6aa1
wkdsh21 <wkdsh21@github.com>
2025-02-03 18:05:56 +0900

    serializer, view 작성

--- tarot/views.py
+++ tarot/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from tarot.serializers import TaroChatContentsInitSerializer
+
 
 # Create your views here.
+class TarotInitViewSet(viewsets.GenericViewSet):
+
+    serializer_class = TaroChatContentsInitSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        # gpt 로직
+        self.get_serializer(data=request.data)
