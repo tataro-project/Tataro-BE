8528649d27fbddcdadc3ec099629e3a2caa019b5
wkdsh21 <wkdsh21@github.com>
2025-02-11 11:32:42 +0900

    develop merge

--- config/settings/base.py
+++ config/settings/base.py
@@ -159,9 +159,6 @@ REST_FRAMEWORK = {
     #     "rest_framework.authentication.BasicAuthentication",
     #     "rest_framework.authentication.SessionAuthentication",
     # ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
 }
 
 
