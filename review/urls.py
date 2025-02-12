3af8ded00bbcd5b3576d086f21723d55483a5c95
youngh0724 <youngh0724@naver.com>
2025-02-12 13:11:25 +0900

    content도 urls.py 수정

--- review/urls.py
+++ review/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from review.views import review_detail_update_delete, review_list_or_create
 
 urlpatterns = [
-    path("reviews/", review_list_or_create, name="review-list-create"),
-    path("reviews/<int:review_id>/", review_detail_update_delete, name="review-detail-update-delete"),
+    path("", review_list_or_create, name="review-list-create"),
+    path("<int:review_id>/", review_detail_update_delete, name="review-detail-update-delete"),
 ]
