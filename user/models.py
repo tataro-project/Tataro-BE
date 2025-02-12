daeacfa9d5331b73f8b6df70a73507f577d26162
sfeedd <qowjdgus86@naver.com>
2025-02-12 19:55:30 +0900

    model 데이터 타입 수정, db 확인

--- user/models.py
+++ user/models.py
@@ -4,10 +4,15 @@ from django.utils import timezone
 
 
 class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # email을 여기서 제거
+
     SOCIAL_CHOICES = [("KAKAO", "Kakao"), ("NAVER", "Naver")]
     GENDER_CHOICES = [("male", "남성"), ("female", "여성")]
 
     id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20, null=True, blank=True)  # type :ignore
+    email = models.EmailField(unique=True)  # email을 unique로 설정
     nickname = models.CharField(max_length=30)
     social_type = models.CharField(max_length=10, choices=SOCIAL_CHOICES)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True)
