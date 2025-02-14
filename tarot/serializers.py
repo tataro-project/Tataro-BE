27c4b238b31af92942db4c7fcc0b1992c423837b
wkdsh21 <wkdsh21@github.com>
2025-02-14 19:12:58 +0900

    tarot generate view clova 모델 변경, 하이퍼 옵셋 변경

--- tarot/serializers.py
+++ tarot/serializers.py
@@ -6,12 +6,15 @@ from .models import TaroCardContents, TaroChatContents, TaroChatRooms
 
 
 class TaroChatContentsInitSerializer(serializers.ModelSerializer[TaroChatContents]):
+    # 직접 외부에서 데이터를 입력
+    chat_id = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         # User 모델과 매핑
         model = TaroChatContents
         # 시리얼라이저에 포함할 필드들을 지정
-        fields = ["content", "created_at", "updated_at"]
-        read_only_fields = ("created_at", "updated_at")
+        fields = ["chat_id", "content", "room_id", "created_at", "updated_at"]
+        read_only_fields = ("created_at", "updated_at", "room_id")
 
     def create(self, validated_data: dict[str, Any]) -> TaroChatContents:
         room_id = self.context.get("room_id")
@@ -23,13 +26,18 @@ class TaroChatContentsInitSerializer(serializers.ModelSerializer[TaroChatContent
         chat_content = TaroChatContents.objects.create(room_id=room_id, **validated_data)
         return chat_content
 
+    # SerializerMethodField에 값 입력
+    def get_chat_id(self, obj: TaroChatContents) -> Any | None:
+        return self.context.get("chat_id")
+
 
 class TaroChatLogSerializer(serializers.Serializer["TaroCardContents"]):
     question = serializers.CharField()
-    content = serializers.ListField(child=serializers.CharField())
+    content = serializers.CharField()
     card_name = serializers.CharField()
     card_url = serializers.URLField()
     card_content = serializers.CharField()
+    card_direction = serializers.CharField()
 
 
 class TaroChatRoomResponseSerializer(serializers.Serializer["TaroChatLogSerializer"]):
