6f2691b5a927b2b4869674d2270464559d7f1d78
wkdsh21 <wkdsh21@github.com>
2025-02-19 18:16:27 +0900

    Payments, Order, Products, BankTransfer model 생성

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from helpers.models import BaseModel
+from user.models import User
+
 
 class Payment(models.Model):
     merchant_id = models.CharField(max_length=255)
@@ -22,3 +25,42 @@ class Payment(models.Model):
 
     def __str__(self) -> str:
         return f"Payment: {self.goods_name} - {self.price}"
+
+
+class Products(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    discount_price = models.IntegerField()
+    img_url = models.URLField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Order(BaseModel):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    count = models.IntegerField(default=1)
+
+
+class BankTransfer(BaseModel):
+    STATUS_CHOICES = (
+        ("pending", "pending"),
+        ("completed", "completed"),
+        ("expired", "expired"),
+        ("canceled", "canceled"),
+        ("mismatch", "mismatch"),
+    )
+    name = models.CharField(max_length=10)
+    deadline = models.DateField()
+    status = models.CharField(choices=STATUS_CHOICES)
+
+
+class Payments(BaseModel):
+    METHOD_CHOICES = (("bank_transfer", "bank_transfer"), ("port_one", "port_one"))
+    STATUS_CHOICES = (("pending", "pending"), ("complete", "complete"), ("failed", "failed"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bank_transfer = models.ForeignKey(BankTransfer, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    method = models.CharField(choices=METHOD_CHOICES)
+    status = models.CharField(choices=STATUS_CHOICES)
