cc42d2b11982e748e9d7a1422cb586cdfb84618a
wkdsh21 <wkdsh21@github.com>
2025-02-24 13:18:55 +0900

    tarot uri 슬래쉬 추가

--- tarot/urls.py
+++ tarot/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path("<int:pk>/", TarotGenerateViewSet.as_view({"post": "create"}), name="tarot_generate"),
     path("logs/", TarotLogViewSet.as_view({"get": "get_all_log"}), name="all_log"),
     path("logs/first/", TarotLogViewSet.as_view({"get": "get_newest_log"}), name="newest_log"),
-    path("logs/<int:pk>/before", TarotLogViewSet.as_view({"get": "get_before_log"}), name="before_log"),
+    path("logs/<int:pk>/before/", TarotLogViewSet.as_view({"get": "get_before_log"}), name="before_log"),
     path("logs/<int:pk>/", TarotLogViewSet.as_view({"get": "get_id_log"}), name="id_log"),
 ]
